1e5a5cd4063e5f26ad4711ec08a8de2bf9e62e2e
Zeynab Jalalian <zjalalianweb@gmail.com>
2026-01-29 13:41:22 +0330

    fix

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,7 +3,7 @@ import "./Banner.css"
 function Banner() {
   return (
     <div>
-       <div className="brand" style={{ backgroundImage: "url('/img/brand.jpeg')"}}>
+       <div className="brand" style={{ backgroundImage: "/react-first-exercise/url('/img/brand.jpeg')"}}>
         <p>"The more that you read, the more things you will know.
             The more that you learn, the more places you'll go."
         </p>
